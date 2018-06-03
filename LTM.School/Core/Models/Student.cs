63d97d115fbe22af0de018ad69783badf4ffed25
张建通 <jtzhang163@163.com>
2018-06-03 16:09:22 +0800

    修改学生详情页代码

--- LTM.School/Core/Models/Student.cs
+++ LTM.School/Core/Models/Student.cs
@@ -1,21 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LTM.School.Core.Models
 {
-    /// <summary>
-    /// 学生
-    /// </summary>
-    public class Student
-    {
-        public int Id { get; set; }
-
-        public string RealName { get; set; }
-
-        public DateTime EnrollmnetDate { get; set; }
-
-        public ICollection<Enrollment> Enrollments { get; set; }
-    }
+  /// <summary>
+  /// 学生
+  /// </summary>
+  public class Student
+  {
+    public int Id { get; set; }
+    [DisplayName("姓名")]
+    public string RealName { get; set; }
+    [DisplayName("注册时间")]
+    public DateTime EnrollmnetDate { get; set; }
+    [DisplayName("登记信息")]
+    public ICollection<Enrollment> Enrollments { get; set; }
+  }
 }
