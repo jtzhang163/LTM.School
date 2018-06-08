8f818402a0265e71c3b9bb23b3d587ecadc2a02f
张建通 <jtzhang163@163.com>
2018-06-08 20:37:52 +0800

    学生信息统计

--- LTM.School/Controllers/HomeController.cs
+++ LTM.School/Controllers/HomeController.cs
@@ -5,33 +5,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using LTM.School.Models;
+using LTM.School.EntityFramework;
+using LTM.School.ViewModels;
 
 namespace LTM.School.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+
+    private readonly SchoolDbContext _context;
+    public HomeController(SchoolDbContext context)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      _context = context;
     }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult About()
+    {
+      ViewData["Message"] = "学生信息统计";
+
+      var entities = from s in _context.Students
+                     group s by s.EnrollmnetDate into g
+                     select new EnrollmentDateGroup { EnrollmentDate = g.Key, Count = g.Count() };
+
+      return View(entities);
+    }
+
+    public IActionResult Contact()
+    {
+      ViewData["Message"] = "Your contact page.";
+
+      return View();
+    }
+
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
 }
---
+++ LTM.School/ViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LTM.School.ViewModels
+{
+  public class EnrollmentDateGroup
+  {
+    /// <summary>
+    /// 学生个数
+    /// </summary>
+    [DisplayName("学生个数")]
+    public int Count { get; set; }
+
+    /// <summary>
+    /// 注册日期
+    /// </summary>
+    [DataType(DataType.Date)]
+    [DisplayName("注册日期")]
+    public DateTime? EnrollmentDate { get; set; }
+  }
+}
